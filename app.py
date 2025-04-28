357c4f654ad034394fe59d9a0362bcaeb948de61
danPoku <dan.gyinaye@gmail.com>
2025-04-28 15:32:29 +0000

    fix: model errors

--- app.py
+++ app.py
@@ -348,7 +348,7 @@ def main():
     # clean & load models once
     climate_df = climate_clean_transform(df_full.copy())
     aq_model = load_model("runs:/e81a7b1389ab485d8b4de63607008f3d/model_artifact")
-    resp_model = load_model("runs:/ff92641b2b744151880019f43b95d92b/model_artifact")
+    resp_model = load_model("runs:/9b84e0378ccf42379b208c11b8116b6e/model_artifact")
 
     # AQ tab
     with aq_tab:
